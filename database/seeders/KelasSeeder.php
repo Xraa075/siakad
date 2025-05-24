18ff69451c41ca7bf42a1babb249bc0f35425243
Xraa075 <phone.poco123@gmail.com>
2025-05-24 21:03:55 +0700

    update seeder

--- database/seeders/KelasSeeder.php
+++ database/seeders/KelasSeeder.php
@@ -14,16 +14,24 @@ class KelasSeeder extends Seeder
         // $jadwalUmum = JadwalKuliah::firstOrCreate(['nama_jadwal' => 'Semester Genap 2024/2025']);
 
         Kelas::create([
-            'nama_kelas' => 'TI-2A',
-            'jurusan_id' => 1, // Asumsi ID Jurusan Teknik Informatika
+            'nama_kelas' => 'D3 TI A',
+            'jurusan_id' => 2, // Asumsi ID Jurusan Teknik Informatika
             'dosen_nip' => 'DSN001', // Asumsi NIP Dosen Budi Santoso sebagai Wali
             'semester' => 2,
             'jadwal_kuliah_id' => 1, // Asumsi ID JadwalKuliah
         ]);
 
         Kelas::create([
-            'nama_kelas' => 'SI-4B',
-            'jurusan_id' => 2, // Asumsi ID Jurusan Sistem Informasi
+            'nama_kelas' => 'D3 TI B',
+            'jurusan_id' => 2, // Asumsi ID Jurusan Teknik Informatika
+            'dosen_nip' => 'DSN002', // Asumsi NIP Dosen Budi Santoso sebagai Wali
+            'semester' => 2,
+            'jadwal_kuliah_id' => 1, // Asumsi ID JadwalKuliah
+        ]);
+
+        Kelas::create([
+            'nama_kelas' => 'D3 SI A',
+            'jurusan_id' => 3, // Asumsi ID Jurusan Sistem Informasi
             'dosen_nip' => 'DSN002', // Asumsi NIP Dosen Siti Aminah sebagai Wali
             'semester' => 4,
             'jadwal_kuliah_id' => 1, // Asumsi ID JadwalKuliah
